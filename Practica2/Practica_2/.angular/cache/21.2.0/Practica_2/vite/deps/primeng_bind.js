dc8083d25c7115e1392134d744356d1743c496d7
ZoeCVt <zoevelaz@outlook.com>
2026-02-26 21:39:04 -0600

    Commit

---
+++ Practica2/Practica_2/.angular/cache/21.2.0/Practica_2/vite/deps/primeng_bind.js
@@ -0,0 +1,11 @@
+import {
+  Bind,
+  BindModule
+} from "./chunk-AB5RQ3P4.js";
+import "./chunk-3PYSYKGX.js";
+import "./chunk-YGTJETDU.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  Bind,
+  BindModule
+};
---
+++ Practica2/Practica_2/.angular/cache/21.2.0/Practica_2/vite/deps/primeng_config.js
@@ -0,0 +1,17 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-VV4XPON7.js";
+import "./chunk-CHDSAFDN.js";
+import "./chunk-3PYSYKGX.js";
+import "./chunk-22O6IK66.js";
+import "./chunk-YGTJETDU.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
